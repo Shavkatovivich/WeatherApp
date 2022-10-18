868a11d3c49969856bce8f49dd2b82247b2f4905
shavkatovivich <frontend6774@gmail.com>
2022-10-18 12:53:48 +0000

    Add weather API and Weather Api Key

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import  "./App.css"
 import CurrentWeather from "./Components/Current-weather/Current-weather";
 import Search from "./Components/Search/Search";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "../src/Components/Api"
 
 const handelOnSearchChange = (searchData) => {
-  console.log(searchData)
+  const [lat, lon] = searchData.value.split("")
+
+  const currentWeatherFeth = fetch(`${WEATHER_API_URL}/weather?lat={lat}&lon={lon}&appid=${WEATHER_API_KEY}`)
 }
 
 function App() {
